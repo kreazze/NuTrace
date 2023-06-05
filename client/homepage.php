02990312031a0555fc380504e9a97a9154133b9f
krease <72684789+kreazze@users.noreply.github.com>
2023-06-05 10:47:36 +0800

    edit homepage

--- client/homepage.php
+++ client/homepage.php
@@ -12,7 +12,7 @@
     <body>   
         <section id="content">
             <nav class="profile">            
-                <img class="logo-pic" src="../assets/images/main/nutrace_logo.png" width="60px" height="60px">
+                <img class="logo-pic" src="../assets/images/main/nutrace_logo.png">
                 <p class="nav-title">NuTrace</p>
             </nav>
             <main>
@@ -20,13 +20,11 @@
                     <h1>Welcome to</h1>
                     <h2>NuTrace!</h2>
                     <h3>A Soil Nutrient Monitoring System</h3>
-                    <p>NuTrace (a compound word from "nutrient" and "trace") is a portable Soil Nutrient Monitoring System. 
+                    <p>NuTrace, a compound word from "<i>Nutrient</i>" and "<i>Trace</i>", is a portable Soil Nutrient Monitoring System. 
                         The NuTrace is a kiosk which primarily features soil nutrient monitoring device. The kiosk holds a 
                         website which offers dashboard, crop harvest inventory, and farm task calendar where farmers can easily 
                         access and monitor their farm records in just several clicks.</p>
-                    <div>
-                        <a href="./dashboard.php"><button class="proceed">PROCEED</button></a>
-                    </div>  
+                    <a href="../account-form/account.php"><button class="proceed">PROCEED</button></a>
                 </div>
                 <div class="home-pic">
                     <div class="bg-img">
